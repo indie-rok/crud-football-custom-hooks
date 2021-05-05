414a96b2bf12f37f827811eaa5c7c89db93d8765
Emmanuel Orozco <indi3.rok@gmail.com>
2021-05-05 04:24:04 +0300

    added teams styling and integration

--- server/data/teams.js
+++ server/data/teams.js
@@ -1,14 +1,14 @@
 module.exports = [
-  { team_name: "Barcelona", team_id: "1", logo: "http://dummyimage.com/3232" },
+  { team_name: "Barcelona", team_id: "1", logo: "http://dummyimage.com/99x49" },
   {
     team_name: "Real Madrid",
     team_id: "2",
-    logo: "http://dummyimage.com/3232",
+    logo: "http://dummyimage.com/100x50",
   },
   {
     team_name: "Manchester U",
     team_id: "3",
     logo: "http://dummyimage.com/3232",
   },
-  { team_name: "Chelsea", team_id: "4", logo: "http://dummyimage.com/3232" },
+  { team_name: "Chelsea", team_id: "4", logo: "http://dummyimage.com/98x50" },
 ];
--- src/screens/Teams/index.tsx
+++ src/screens/Teams/index.tsx
@@ -1,6 +1,29 @@
 import { useEffect, useState } from "react";
+import { Card, Button, Col, Row, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import { getTeams } from "../../api/Teams";
+
+interface Team {
+  team_id: string;
+  team_name: string;
+  logo: string;
+}
+
+const renderTeams = (teams: Team[]) =>
+  teams.map(({ team_id, team_name, logo }) => (
+    <Col>
+      <Card style={{ width: "18rem" }} className="">
+        <Card.Img variant="top" src={logo} />
+        <Card.Body>
+          <Card.Title>{team_name}</Card.Title>
+          <Button as={Link} to={`/team/${team_id}`} variant="primary">
+            Details
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  ));
 
 export default function Teams() {
   const [teams, setTeams] = useState([]);
@@ -8,13 +31,17 @@ export default function Teams() {
   useEffect(() => {
     async function fetchApi() {
       const response = await getTeams();
-      console.log(response);
-      console.log(teams);
       setTeams(response);
     }
 
     fetchApi();
   }, []);
 
-  return <div>Teams</div>;
+  return (
+    <>
+      <Container>
+        <Row>{renderTeams(teams)}</Row>
+      </Container>
+    </>
+  );
 }
